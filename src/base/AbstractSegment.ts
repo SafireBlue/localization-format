b7d894d73352e7f3f3eaebd6e67351d2143f2285
Akira Ogawa <loc_engineer@icloud.com>
2019-08-24 23:13:38 +0900

    Misc updates

--- src/base/AbstractSegment.ts
+++ src/base/AbstractSegment.ts
@@ -2,20 +2,8 @@ import ISegment from "./ISegment";
 import IText from "./IText";
 
 export default abstract class AbstractSegment implements ISegment {
-    public get Source(): string | IText {
-        return this.source;
-    }
-    public set Source(value: string | IText) {
-        this.source = value;
-    }
-    public get Translation(): string | IText {
-        return this.translation;
-    }
-    public set Translation(value: string | IText) {
-        this.translation = value;
-    }
     public FormatIndex!: number | null;
     public Props!: { [index: string]: string; } | null;
-    private source!: string | IText;
-    private translation!: string | IText;
+    public Source!: string | IText;
+    public Translation!: string | IText;
 }
--- src/tmx/TMXFormatParser.ts
+++ src/tmx/TMXFormatParser.ts
@@ -18,9 +18,15 @@ export default class TMXFormatParser implements ILocFormatParser<TMXSegment> {
             }
             const seg = new TMXSegment();
             seg.FormatIndex = i;
-            seg.Source = isObject(t.tuv[0].seg[0]) ? matches[i * 2].substring("<seg>".length) : t.tuv[0].seg[0];
+            seg.Source = {
+                BeginOffSet: null,
+                Value: isObject(t.tuv[0].seg[0]) ? matches[i * 2].substring("<seg>".length) : t.tuv[0].seg[0],
+            };
             // tslint:disable-next-line:max-line-length
-            seg.Translation = isObject(t.tuv[1].seg[0]) ? matches[(i * 2 + 1)].substring("<seg>".length) : t.tuv[1].seg[0],
+            seg.Translation = {
+                BeginOffSet: null,
+                Value: isObject(t.tuv[1].seg[0]) ? matches[(i * 2 + 1)].substring("<seg>".length) : t.tuv[1].seg[0],
+            },
             seg.Props = Props;
             (result as TMXSegment[]).push(seg);
         });
